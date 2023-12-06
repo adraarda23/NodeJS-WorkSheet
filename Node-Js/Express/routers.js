3ac09c668ca98d810985bac68bcb65d288dd2ee2
adraarda23 <ardaaydinkilinc@gmail.com>
2023-12-06 06:21:46 +0300

    express routers

--- Node-Js/Express/routers.js
+++ Node-Js/Express/routers.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const app = express();
 
+app.get("/admin",(req,res,next)=>{
+    console.log("Admin Page");
+    res.send("admin page");
+});
 
 app.get("/",(req,res,next)=>{
-    console.log("In a middleware");
-    next();
+    console.log("Home Page");
+    res.send("homepage");
 });
-app.get("/",(req,res,next)=>{
-    console.log("In a another middleware");
-})
+
 
 
 app.listen(3000,()=>{
     console.log("3000 portunda çalışıyor");
-})
+});
