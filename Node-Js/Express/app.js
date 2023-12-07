8b441489d21d806c854cd92addcd686c96b1742a
adraarda23 <ardaaydinkilinc@gmail.com>
2023-12-07 07:30:41 +0300

    pug finished

--- Node-Js/Express/app.js
+++ Node-Js/Express/app.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const userRouter = require("./routes/shop");
-const adminData = require("./routes/admin");
-const dirRoot = require("./util/path");
+const bodyParser = require('body-parser');
 app.set('view engine', 'pug');
 app.set('views', 'views');
-app.use(userRouter);
-app.use("/admin",adminData.router);
-app.use(express.static(path.join(__dirname,"public")));
+const userRouter = require("./routes/shop");
+const adminData = require("./routes/admin");
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(dirRoot,"views","notfound.html"));
-})
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(userRouter);
+app.use("/admin",adminData.routes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
+});
 
 app.listen(3000,()=>{
     console.log("3000 portunda çalışıyor");
     console.log(path.join(__dirname,"public"));
 })
+
+
+
+
--- Node-Js/Express/routes/admin.js
+++ Node-Js/Express/routes/admin.js
@@ -3,21 +3,21 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+
 const router = express.Router();
+
 const products = [];
 
-const bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({ extended: false }));
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  products.push({title:req.body.title});
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-exports.router=router;
-exports.products=products;
+exports.routes = router;
+exports.products = products;
