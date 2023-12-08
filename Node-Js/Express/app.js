aabf9ddc37b35b54b5a49f1e18445ad733313488
adraarda23 <ardaaydinkilinc@gmail.com>
2023-12-08 06:26:36 +0300

    Model View Controller

--- Node-Js/Express/app.js
+++ Node-Js/Express/app.js
@@ -5,16 +5,15 @@ const bodyParser = require('body-parser');
 app.set('view engine', 'pug');
 app.set('views', 'views');
 const userRouter = require("./routes/shop");
-const adminData = require("./routes/admin");
+const adminRouter = require("./routes/admin");
+const errorRouter = require("./controllers/error");
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(userRouter);
-app.use("/admin",adminData.routes);
+app.use("/admin",adminRouter);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
+app.use(errorRouter.get404);
 
 app.listen(3000,()=>{
     console.log("3000 portunda çalışıyor");
