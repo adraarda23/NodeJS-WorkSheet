a1bd0620539e72b890b4f428c4492417eda40dfe
adraarda23 <ardaaydinkilinc@gmail.com>
2023-12-09 06:28:40 +0300

    due to sychronization with maximillian schwarzmüller his files and hierarchy copied

--- Node-Js/Express/app.js
+++ Node-Js/Express/app.js
@@ -1,25 +1,24 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+const path = require('path');
+
+const express = require('express');
 const bodyParser = require('body-parser');
-app.set('view engine', 'pug');
+
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
-const userRouter = require("./routes/shop");
-const adminRouter = require("./routes/admin");
-const errorRouter = require("./controllers/error");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname,"public")));
-app.use(userRouter);
-app.use("/admin",adminRouter);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(errorRouter.get404);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000,()=>{
-    console.log("3000 portunda çalışıyor");
-    console.log(path.join(__dirname,"public"));
-})
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
+app.use(errorController.get404);
 
-
-
+app.listen(3000);
--- Node-Js/Express/models/product.js
+++ Node-Js/Express/models/product.js
@@ -1,63 +1,40 @@
 const fs = require('fs');
 const path = require('path');
-const rootDir=require("../util/path");
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    const p = path.join(
-      rootDir,
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        console.error('Error reading file:', err);
-        return;
-      }
-
-      let products = [];
-      if (fileContent.length > 0) {
-        try {
-          products = JSON.parse(fileContent);
-        } catch (parseError) {
-          console.error('Error parsing JSON:', parseError);
-          return;
-        }
-      }
-
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        if (err) {
-          console.error('Error writing file:', err);
-        }
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
       });
     });
   }
 
-  static fetchAll(callback) {
-    const p = path.join(
-      rootDir,
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        console.error('Error reading file:', err);
-        callback([]);
-        return;
-      }
-
-      try {
-        const products = JSON.parse(fileContent);
-        callback(products);
-      } catch (parseError) {
-        console.error('Error parsing JSON:', parseError);
-        callback([]);
-      }
-    });
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
   }
 };
