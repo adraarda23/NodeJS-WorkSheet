521ba75a240456e679482f8a209414f683bae1d9
adraarda23 <ardaaydinkilinc@gmail.com>
2023-12-20 19:16:38 +0300

    flash

--- Node-Js/Express/app.js
+++ Node-Js/Express/app.js
@@ -1,5 +1,6 @@
 
 const path = require('path');
+const flash = require("connect-flash");
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -40,6 +41,7 @@ app.use(
   })
 );
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
